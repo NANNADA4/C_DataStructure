3ce0ab6cd19877d9f4b21dd6a1ee79e527e7db7b
nannada4 <coco96062233@gmail.com>
2021-04-08 11:59:28 +0900

    Add HW#05, HW#06

---
+++ HW/HW06/HW6-2/LinkedStackMain.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "LinkedStack.h"
+
+int main() {
+	Stack* S;
+	element item;
+	int i;
+	
+	printf("(1) 공백 스택 생성하기\n");
+	S = create();
+	displayStack(S);
+	
+	printf("\n(2) 스택에 10, 20, 30을 차례로 삽입\n");
+	// Fill your code
+
+
+
+
+	
+	printf("\n(3) 현재 top의 요소를 출력\n");
+	// Fill your code
+
+
+
+
+	
+	// Fill your code
+
+
+
+
+
+	
+	printf("\n(5) 스택 비우기\n");
+	clear(S);
+	displayStack(S);
+	
+	return 0;
+}
+
