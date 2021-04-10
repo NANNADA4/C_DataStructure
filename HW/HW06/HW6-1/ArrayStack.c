60b908858d381f147d3c053fa173a3ebde32fc06
nannada4 <coco96062233@gmail.com>
2021-04-10 14:49:28 +0900

    Finish HW#06-1

--- HW/HW06/HW6-1/ArrayStack.c
+++ HW/HW06/HW6-1/ArrayStack.c
@@ -13,26 +13,49 @@ Stack* create() {
 // 스택이 포화상태인지 확인하는 연산
 int isFull(Stack* S) {
     // Fill your code
+    return S->top == MAX_SIZE - 1;
 }
 
 // 스택이 공백상태인지 확인하는 연산
 int isEmpty(Stack* S) {
     // Fill your code
+    return S->top == -1;
 }
 
 // 스택의 top에 요소를 추가하는 연산
 void push(Stack* S, element x) {
     // Fill your code
+    if (isFull(S)) {
+        printf("[ERROR] Stack is Full! \n");
+        return;
+    } else {
+        S->top += 1;
+        S->stack[(S->top)] = x;
+    }
 }
 
 // 스택에서 마지막에 저장된 요소 삭제 및 반환하는 연산
 element pop(Stack* S) {
     // Fill your code
+    if (isEmpty(S)) {
+        printf("[ERROR] Stack is Empty \n");
+        return;
+    } else {
+        element e = S->stack[S->top];
+        S->top -= 1;
+        return e;
+    }
 }
 
 // 스택에서 마지막에 저장된 요소를 반환하는 연산
 element peek(Stack* S) {
     // Fill your code
+    if (isEmpty(S)) {
+        printf("[ERROR] Stack is Empty \n");
+        return;
+    } else {
+        return S->stack[S->top];
+    }
 }
 
 // 스택의 모든 요소 출력
